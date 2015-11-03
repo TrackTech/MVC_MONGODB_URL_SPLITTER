b72152312ac209baf4b19e18b7b2800dc6b54a1e
TrackTech <TrackTech09@gmail.com>
2015-11-02 16:42:17 -0800

    C# MVC to MONGODB

---
+++ Classess/DBAccess.cs
@@ -0,0 +1,80 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace URL_Splitter.DAL
+{
+    public static class DBAccess
+    {
+        private static IMongoClient _client;
+        private static IMongoDatabase _database;
+        const string connectionString = "mongodb://localhost:27017";
+        private static object _object=new object();
+
+        public static string AddDocument(string url, string tagName)
+        {
+            try {
+                InitDB();
+                _database = _client.GetDatabase("urldb");
+                var collection = _database.GetCollection<BsonDocument>("urlqs");
+                FilterDefinition<BsonDocument> filter = new BsonDocument(new BsonElement("url", url.ToLower()));
+
+                var countTask = collection.CountAsync(filter);
+
+                countTask.Wait();             
+                if(countTask.Result>0)
+                {
+                    return "URL Rejected: Duplicate";
+                }
+                var document = new BsonDocument {
+                                                    { "url",url.ToLower()}, {"tagname",tagName.ToLower()}, { "timestamp",DateTime.Now}
+                                                };
+                
+                var qsVariables = ParseQueryString(url);
+                foreach (string key in qsVariables)
+                {
+                    var value = qsVariables[key];
+                    document.Add(new BsonElement(key.ToLower(), value.ToLower()));
+                }
+
+                var InsertTask =  collection.InsertOneAsync(document);
+                InsertTask.Wait();
+                return "URL Successfully Recorded";
+            }
+            catch(Exception ex)
+            {
+                return "Exception Occured";
+            }
+        }
+        private static NameValueCollection ParseQueryString(string url)
+        {
+            String querystring;
+            int iqs = url.IndexOf('?');
+            if (iqs == -1)
+            {
+                return null;
+            }
+            else
+            {
+                querystring = (iqs < url.Length - 1) ? url.Substring(iqs + 1) : String.Empty;
+                return HttpUtility.ParseQueryString(querystring);
+            }
+        }
+        
+        private static void InitDB()
+        {
+            if (_client == null)
+            {
+                lock(_object)
+                {
+                    if(_client==null)
+                    {
+                        _client = new MongoClient(connectionString);
+                    }
+                }
+            }
+        }
+    } 
+}
---
+++ Controller/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using URL_Splitter.DAL;
+
+namespace URL_Splitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult Index(string txtTagName, string txtUrl)
+        {
+            if (string.IsNullOrEmpty(txtTagName))
+                txtTagName = "UnKnown";
+            ViewBag.TagName = txtTagName;
+            if (string.IsNullOrEmpty(txtUrl))
+            {
+                ViewBag.ControllerMessage = "Url Rejected";
+                return View();
+            }
+
+            ViewBag.ControllerMessage = DBAccess.AddDocument(txtUrl, txtTagName);
+
+            return View();
+        }
+    
+    }
+}
